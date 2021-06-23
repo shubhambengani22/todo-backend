4cb360cee2e45d15b95a912b0ef876f0ffc74bc1
Shubham Bengani <shubhambengani22@gmail.com>
2021-06-23 19:02:49 +0530

    Adding the test cases for Update TODO

--- test/spec/todo-item/todo-item.spec.ts
+++ test/spec/todo-item/todo-item.spec.ts
@@ -90,4 +90,63 @@ describe('POST /todos', () => {
         .to.equal('The title is empty or the title is not a string.')
     }
   })
+
+  describe('PUT /todos/:id', () => {
+    it('should update a todo item if it exists, if id is valid mongo id and if title is valid non-empty string', async () => {
+      const todoitem = await testAppContext.todoItemRepository.save(
+        new TodoItem({ title: 'Update TODO' })
+      )
+
+      const res = await chai
+        .request(expressApp)
+        .put(`/todos/${todoitem._id}`)
+        .send({
+          title: 'Item Updated',
+        })
+
+      expect(res).to.have.status(200)
+      expect(res.body).to.have.property('id')
+      expect(res.body).to.have.property('title')
+    })
+
+    it('should return a validation error if empty title is specified', async () => {
+      const todoitem = await testAppContext.todoItemRepository.save(
+        new TodoItem({ title: 'TODO_TO_BE_UPDATED' })
+      )
+
+      const res = await chai
+        .request(expressApp)
+        .put(`/todos/${todoitem._id}`)
+        .send({
+          title: '',
+        })
+
+      expect(res).to.have.status(400)
+      expect(res.body)
+        .to.have.nested.property('failures[0].message')
+        .to.equal('The title is empty or the title is not a string.')
+    })
+
+    it('should return a validation error if id is invalid mongo id', async () => {
+      const res = await chai.request(expressApp).put('/todos/hhd8882nn').send({
+        title: 'Update TODO',
+      })
+
+      expect(res).to.have.status(400)
+      expect(res.body)
+        .to.have.nested.property('failures[0].message')
+        .to.equal('The specified ID is not a MongoDB ID.')
+    })
+
+    it('should return a 404 if todo item does not exists', async () => {
+      const res = await chai
+        .request(expressApp)
+        .put('/todos/605bb3efc93d78b7f4335c2c')
+        .send({
+          title: 'TODO_UPDATED',
+        })
+
+      expect(res).to.have.status(404)
+    })
+  })
 })
