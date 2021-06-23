df52001baf3fdaf64c32eb150061ed01bb5491cd
Shubham Bengani <shubhambengani22@gmail.com>
2021-06-23 19:21:13 +0530

    Adding Fetch TODO Feature

--- src/controllers/todo-item-controller.ts
+++ src/controllers/todo-item-controller.ts
@@ -8,7 +8,7 @@ import {
   ExtendedRequest,
   ValidationFailure,
 } from '@typings'
-import { createTodoItemValidator } from '@validators'
+import { createTodoItemValidator, fetchTodoItemValidator } from '@validators'
 
 export class TodoItemController extends BaseController {
   public basePath: string = '/todos'
@@ -24,6 +24,12 @@ export class TodoItemController extends BaseController {
       `${this.basePath}`,
       createTodoItemValidator(this.appContext),
       this.createTodoItem
+    )
+
+    this.router.get(
+      `${this.basePath}/:id`,
+      fetchTodoItemValidator(this.appContext),
+      this.fetchTodoItem
     )
   }
 
@@ -47,5 +53,35 @@ export class TodoItemController extends BaseController {
       new TodoItem({ title })
     )
     res.status(201).json(todoItem.serialize())
+  }
+
+  private fetchTodoItem = async (
+    req: ExtendedRequest,
+    res: Response,
+    next: NextFunction
+  ) => {
+    const failures: ValidationFailure[] =
+      Validation.extractValidationErrors(req)
+    if (failures.length > 0) {
+      const valError = new Errors.ValidationError(
+        res.__('DEFAULT_ERRORS.VALIDATION_FAILED'),
+        failures
+      )
+      return next(valError)
+    }
+
+    const { id } = req.params
+    const todoItem = await this.appContext.todoItemRepository.findOne({
+      _id: id,
+    })
+
+    if (todoItem._id) {
+      res.status(200).json(todoItem.serialize())
+    } else {
+      const valError = new Errors.NotFoundError(
+        res.__('DEFAULT_ERRORS.VALIDATION_FAILED')
+      )
+      next(valError)
+    }
   }
 }
