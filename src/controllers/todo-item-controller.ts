8085ec36ccca137540641c0c425599ca9d380b74
shubhambengani22 <shubhambengani22@gmail.com>
2021-06-27 16:53:27 +0530

    Delete TODO Feature (#2)
    * Adding Delete TODO Feature
    
    * Minor changes to en.json
    
    * Adding Update TODO Feature

--- src/controllers/todo-item-controller.ts
+++ src/controllers/todo-item-controller.ts
@@ -1,22 +1,27 @@
-import { BaseController } from './base-controller'
-import { NextFunction, Response, Router } from 'express'
-import { Validation } from '@helpers'
-import { TodoItem } from '@models'
+import { BaseController } from "./base-controller";
+import { NextFunction, Response, Router } from "express";
+import { Validation } from "@helpers";
+import { TodoItem } from "@models";
 import {
   AppContext,
   Errors,
   ExtendedRequest,
   ValidationFailure,
-} from '@typings'
-import { createTodoItemValidator, updateTodoItemValidator } from '@validators'
+} from "@typings";
+
+import {
+  createTodoItemValidator,
+  deleteTodoItemValidator,
+  updateTodoItemValidator,
+} from "@validators";
 
 export class TodoItemController extends BaseController {
-  public basePath: string = '/todos'
-  public router: Router = Router()
+  public basePath: string = "/todos";
+  public router: Router = Router();
 
   constructor(ctx: AppContext) {
-    super(ctx)
-    this.initializeRoutes()
+    super(ctx);
+    this.initializeRoutes();
   }
 
   private initializeRoutes() {
@@ -24,13 +29,18 @@ export class TodoItemController extends BaseController {
       `${this.basePath}`,
       createTodoItemValidator(this.appContext),
       this.createTodoItem
-    )
+    );
 
+    this.router.delete(
+      `${this.basePath}/:id`,
+      deleteTodoItemValidator(this.appContext),
+      this.deleteTodoItem
+    );
     this.router.put(
       `${this.basePath}/:id`,
       updateTodoItemValidator(this.appContext),
       this.updateTodoItem
-    )
+    );
   }
 
   private createTodoItem = async (
@@ -39,21 +49,43 @@ export class TodoItemController extends BaseController {
     next: NextFunction
   ) => {
     const failures: ValidationFailure[] =
-      Validation.extractValidationErrors(req)
+      Validation.extractValidationErrors(req);
     if (failures.length > 0) {
       const valError = new Errors.ValidationError(
-        res.__('DEFAULT_ERRORS.VALIDATION_FAILED'),
+        res.__("DEFAULT_ERRORS.VALIDATION_FAILED"),
         failures
-      )
-      return next(valError)
+      );
+      return next(valError);
     }
 
-    const { title } = req.body
+    const { title } = req.body;
     const todoItem = await this.appContext.todoItemRepository.save(
       new TodoItem({ title })
-    )
-    res.status(201).json(todoItem.serialize())
-  }
+    );
+    res.status(201).json(todoItem.serialize());
+  };
+
+  private deleteTodoItem = async (
+    req: ExtendedRequest,
+    res: Response,
+    next: NextFunction
+  ) => {
+    const failures: ValidationFailure[] =
+      Validation.extractValidationErrors(req);
+    if (failures.length > 0) {
+      const valError = new Errors.ValidationError(
+        res.__("DEFAULT_ERRORS.VALIDATION_FAILED"),
+        failures
+      );
+      return next(valError);
+    }
+
+    const { id } = req.params;
+    const deleteItem = await this.appContext.todoItemRepository.deleteMany({
+      _id: id,
+    });
+    res.status(204).json(deleteItem);
+  };
 
   private updateTodoItem = async (
     req: ExtendedRequest,
@@ -61,29 +93,29 @@ export class TodoItemController extends BaseController {
     next: NextFunction
   ) => {
     const failures: ValidationFailure[] =
-      Validation.extractValidationErrors(req)
+      Validation.extractValidationErrors(req);
     if (failures.length > 0) {
       const valError = new Errors.ValidationError(
-        res.__('DEFAULT_ERRORS.VALIDATION_FAILED'),
+        res.__("DEFAULT_ERRORS.VALIDATION_FAILED"),
         failures
-      )
-      return next(valError)
+      );
+      return next(valError);
     }
 
-    const { id } = req.params
-    const { title } = req.body
+    const { id } = req.params;
+    const { title } = req.body;
     const todoItem = await this.appContext.todoItemRepository.update(
       { _id: id },
       { $set: { title } }
-    )
+    );
 
     if (todoItem._id) {
-      res.status(200).json(todoItem.serialize())
+      res.status(200).json(todoItem.serialize());
     } else {
       const valError = new Errors.NotFoundError(
-        res.__('DEFAULT_ERRORS.VALIDATION_FAILED')
-      )
-      next(valError)
+        res.__("DEFAULT_ERRORS.VALIDATION_FAILED")
+      );
+      next(valError);
     }
-  }
+  };
 }
